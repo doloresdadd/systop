1f52242501f545d5a3f1cc25574649cb7c3c3b81
Bethanie Innes <doloresdadd@gmail.com>
2020-06-19 20:13:27 +1000

    Add notifications

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -4,6 +4,44 @@ const cpu = osu.cpu;
 const mem = osu.mem;
 const os = osu.os;
 
+let cpuOverload = 5;
+let alertFrequency = 1;
+
+// Run every 2 seconds
+setInterval(() => {
+  //CPU Usage
+  cpu.usage().then((info) => {
+    document.getElementById("cpu-usage").innerText = info + "%";
+
+    document.getElementById("cpu-progress").style.width = info + "%";
+
+    //Make progess bar red if overload
+    if (info >= cpuOverload) {
+      document.getElementById("cpu-progress").style.background = "red";
+    } else {
+      document.getElementById("cpu-progress").style.background = "#30c88b";
+    }
+
+    //Check Overload
+    if (info >= cpuOverload && runNotify(alertFrequency)) {
+      notifyUser({
+        title: "CPU Overload",
+        body: `CPU is over ${cpuOverload}%`,
+        icon: path.join(__dirname, "img", "icon.png"),
+      });
+
+      localStorage.setItem("lastNotify", +new Date());
+    }
+  });
+
+  cpu.free().then((info) => {
+    document.getElementById("cpu-free").innerText = info + "%";
+  });
+
+  //Uptime
+  document.getElementById("sys-uptime").innerText = secondsTodhms(os.uptime());
+}, 2000);
+
 //  Set Model
 document.getElementById("cpu-model").innerText = cpu.model();
 
@@ -18,3 +56,37 @@ document.getElementById("os").innerText = `${os.type()} ${os.arch()}`;
 mem.info().then((info) => {
   document.getElementById("mem-total").innerText = info.totalMemMb;
 });
+
+//Show days hours minutes seconds
+function secondsTodhms(seconds) {
+  seconds = +seconds;
+  const d = Math.floor(seconds / (3600 * 24));
+  const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = Math.floor(seconds % 60);
+  return `${d}d, ${h}h, ${m}m, ${s}s`;
+}
+
+//Send notification
+function notifyUser(options) {
+  new Notification(options.title, options);
+}
+
+//Check how much time since last notification
+function runNotify(frequency) {
+  if (localStorage.getItem("lastNotify") === null) {
+    //Store time
+    localStorage.setItem("lastNotify", +new Date());
+    return true;
+  }
+  const notifyTime = new Date(parseInt(localStorage.getItem("lastNotify")));
+  const now = new Date();
+  const diffTime = Math.abs(now - notifyTime);
+  const minutesPast = Math.ceil(diffTime / (1000 * 60));
+
+  if (minutesPast > frequency) {
+    return true;
+  } else {
+    return false;
+  }
+}
